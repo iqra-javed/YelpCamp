2d799113f3207afa043284adc4f6704cfa56a228
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2018-12-07 03:58:57 -0600

    set up post route & form to create new campground

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 const campgrounds = [
   {
@@ -19,14 +23,25 @@ const campgrounds = [
   }
 ];
 
-app.set('view engine', 'ejs');
-
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', {campgrounds: campgrounds});
+  res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+app.post('/campgrounds', (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = { name, image };
+  campgrounds.push(newCampground);
+
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new.ejs');
 });
 
 app.listen(3000, () => {
